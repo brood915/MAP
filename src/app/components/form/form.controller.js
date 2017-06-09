7795ab0587f96f8c936ff6763c6b19d1d49c662f
k <brood915@gmail.com>
2017-06-09 11:52:18 -0400

    added form validation

--- src/app/components/form/form.controller.js
+++ src/app/components/form/form.controller.js
@@ -1,15 +1,21 @@
 import angular from 'angular';
 
 export default class FormCtrl {
-  constructor(booksService) {
+  constructor(booksService, $timeout, $scope) {
     this.booksService = booksService;
     this.newBook = {};
+    this.$timeout = $timeout;
+    this.$scope = $scope;
   }
   handleClick() {
     let newBook = angular.copy(this.newBook); //deep copy to remove model-view binding
     this.booksService.books.push(newBook);
-    this.newBook = {};
+
+    this.$timeout(() => { //to reset the form to remove warning msgs + empty the inputs
+      this.newBook = {};
+      this.$scope.myForm.$setPristine(); 
+    }); 
   }
 }
 
- FormCtrl.$inject = ['booksService']; 
+ FormCtrl.$inject = ['booksService', '$timeout' ,'$scope']; 
