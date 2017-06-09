f78e12ad7a46f62d78a39a192072468c457bfd5d
brood915 <brood915@gmail.com>
2017-06-09 00:20:47 -0400

    added polyfill for html5 DnD for mobiles

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../assets/style/main.scss';
 import components from './components/components.module';
 import appDirective from './app.directive';
+import 'drag-drop-webkit-mobile'; //polyfill for mobiles
 
 angular.module('app', [components])
   .directive('app', appDirective)
