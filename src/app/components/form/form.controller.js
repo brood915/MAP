7716a83800390f9afb2572b036e5493fec115a36
brood915 <brood915@gmail.com>
2017-06-10 13:09:57 -0400

    fixed bugs with sorting date

--- src/app/components/form/form.controller.js
+++ src/app/components/form/form.controller.js
@@ -1,14 +1,17 @@
 import angular from 'angular';
 
 export default class FormCtrl {
-  constructor(booksService, $timeout, $scope) {
+  constructor(booksService, $timeout, $scope, $filter) {
     this.booksService = booksService;
     this.newBook = {};
     this.$timeout = $timeout;
     this.$scope = $scope;
+    this.$filter = $filter;
   }
   handleClick() {
     let newBook = angular.copy(this.newBook); //deep copy to remove model-view binding
+    let formatDate = this.$filter('date');
+    newBook.date = formatDate(newBook.date, 'mediumDate');
     this.booksService.books.push(newBook);
 
     this.$timeout(() => { //to reset the form to remove warning msgs + empty the inputs
@@ -18,4 +21,4 @@ export default class FormCtrl {
   }
 }
 
- FormCtrl.$inject = ['booksService', '$timeout' ,'$scope']; 
+ FormCtrl.$inject = ['booksService', '$timeout' ,'$scope', '$filter']; 
